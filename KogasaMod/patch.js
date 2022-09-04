c1a59046f4e4d228833bbb9fd43cff3dfaa0b0cf
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2022-09-04 19:29:58 -0400

    i hate the space bar

--- KogasaMod/patch.js
+++ KogasaMod/patch.js
@@ -4,9 +4,9 @@
 		"nmlgc/script_latin",
 		"nmlgc/western_name_order"
 	],
-	"id": "Kogasa Mod",
+	"id": "KogasaMod",
 	"servers": [
-		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/Kogasa Mod/"
+		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/KogasaMod/"
 	],
 	"title": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night"
 }
--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "Kogasa📈#8100",
 	"id": "Kogasa",
 	"patches": {
-		"Kogasa Mod": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night"
+		"KogasaMod": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/"
