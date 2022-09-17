e7a18e76efef2d19722840b39b5c3566a6843085
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2022-09-16 20:38:05 -0400

    rinsatsuki

---
+++ RinSatsukiMod/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "INRandomMod",
+	"servers": [
+		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/"
+	],
+	"title": "RinSatsuki (06) Mod that adds Rin Satsuki to Touhou EOSD Embodiment of Scarlet Devil"
+}
--- repo.js
+++ repo.js
@@ -3,7 +3,8 @@
 	"id": "Kogasa",
 	"patches": {
 		"INRandomMod": "INRandom (08) Mod that adds the Aki sisters, Nitori, Takane, Kaguya, and Doremy to Touhou 08 Imperishable Night",
-		"KogasaMod": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night"
+		"KogasaMod": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night",
+		"RinSatsuki (06) Mod that adds Rin Satsuki to Touhou EOSD Embodiment of Scarlet Devil"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/"
