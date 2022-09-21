dbb8a955a30c95f0570693857a70710abd75ae82
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2022-09-20 21:08:24 -0400

    okuu

---
+++ OkuuMod/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "OkuuMod",
+	"servers": [
+		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/OkuuMod/"
+	],
+	"title": "OkuuMod (17) Mod that adds Utsuho Reiuji to Touhou WBaWC."
+}
--- repo.js
+++ repo.js
@@ -4,7 +4,7 @@
 	"patches": {
 		"INRandomMod": "INRandom (08) Mod that adds the Aki sisters, Nitori, Takane, Kaguya, and Doremy to Touhou 08 Imperishable Night",
 		"KogasaMod": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night",
-		"OkuuMod": "RinSatsuki (06) Mod that adds Rin Satsuki to Touhou EOSD Embodiment of Scarlet Devil",
+		"OkuuMod": "OkuuMod (17) Mod that adds Utsuho Reiuji to Touhou WBaWC.",
 		"RinSatsukiMod": "RinSatsuki (06) Mod that adds Rin Satsuki to Touhou EOSD Embodiment of Scarlet Devil",
 		"YukariCtcMod": "YukariCtc (07) Mod that replaces Yukari's PCB theme with the CTC Night Falls ~ Evening Star."
 	},
