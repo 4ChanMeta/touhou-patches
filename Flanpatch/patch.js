52ffb5965b99e5c83836147438a97f84f0d44ec5
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2022-10-02 21:41:06 -0400

    raymoo

---
+++ Flanpatch/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "Flanpatch",
+	"servers": [
+		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/Flanpatch/"
+	],
+	"title": "Flanpatch (11) Mod that adds Flandre Scarlet as playable to Touhou SA Subterranean Animism"
+}
--- repo.js
+++ repo.js
@@ -6,6 +6,7 @@
 		"INRandomMod": "INRandom (08) Mod that adds the Aki sisters, Nitori, Takane, Kaguya, and Doremy to Touhou 08 Imperishable Night",
 		"KogasaMod": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night",
 		"OkuuMod": "OkuuMod (17) Mod that adds Utsuho Reiuji to Touhou WBaWC.",
+		"ReimuHsifs": "ReimuHsifs (06-18) Mod that makes Reimu's homing bullets look like the ones from Hsifs.",
 		"RinSatsukiMod": "RinSatsuki (06) Mod that adds Rin Satsuki to Touhou EOSD Embodiment of Scarlet Devil",
 		"YukariCtcMod": "YukariCtc (07) Mod that replaces Yukari's PCB theme with the CTC Night Falls ~ Evening Star."
 	},
