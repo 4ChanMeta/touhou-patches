6931e65d68c8dc933b739b4b68c15cb2b8bd6be9
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2023-04-20 16:30:22 -0400

    funny case sensitive

--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
 	"id": "Kogasa",
 	"patches": {
 		"EnemyNotification": "EnemyNotification (13-18, 14.3) Mod that makes the enemy marker more visible in some of the later games.",
-		"FlanPatch": "FlanPatch (11) (WIP) Mod that makes Flandre playable in SA.",
+		"Flanpatch": "FlanPatch (11) (WIP) Mod that makes Flandre playable in SA.",
 		"INRandomMod": "INRandom (08) Mod that adds the Aki sisters, Nitori, Takane, Kaguya, and Doremy to Touhou 08 Imperishable Night",
 		"KogasaMod": "Kogasa Mod (08) Mod that adds Kogasa, Koishi, Satori, Mugetsu, and Gengetsu to Touhou 08 Imperishable Night",
 		"OkuuMod": "OkuuMod (17) Mod that adds Utsuho Reiuji to Touhou WBaWC.",
