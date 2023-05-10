778e4a837eaafc2465a9c4524bef4c535bd2a338
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2023-05-10 18:06:36 -0400

    what a funky

--- repo_update.bat
+++ repo_update.bat
@@ -3,7 +3,7 @@ if "%*"=="" (
 echo Commit message is empty^^!
 exit /b
 )
-py -3 "..\..\bin\scripts\repo_update.py"
+py -3 "..\..\..\bin\scripts\repo_update.py" > "log.txt" 2>&1 
 git add .
 git commit -m "%*"
 git push
