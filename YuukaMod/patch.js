c3059f156992f57f8c1bd4d748c0c00c66b72ea8
4ChanMeta <56356893+4ChanMeta@users.noreply.github.com>
2023-05-10 16:36:28 -0400

    y00ka

---
+++ YuukaMod/patch.js
@@ -0,0 +1,12 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order"
+	],
+	"id": "RinSatsukiMod",
+	"servers": [
+		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/YuukaMod/"
+	],
+	"title": "YuukaMod (06) Mod that makes Yuuka playable in EoSD Embodiment of Scarlet Devil"
+}
--- repo.js
+++ repo.js
@@ -10,7 +10,8 @@
 		"ReimuHsifs": "ReimuHsifs (06-18) Mod that makes Reimu's homing bullets look like the ones from Hsifs.",
 		"ReimuHsifsPlus": "ReimuHsifsPlus (13-18) ReimuHsifs mod but makes the bullets behave like hsifs too (perma homing).",
 		"RinSatsukiMod": "RinSatsuki (06) Mod that adds Rin Satsuki to Touhou EOSD Embodiment of Scarlet Devil",
-		"YukariCtcMod": "YukariCtc (07) Mod that replaces Yukari's PCB theme with the CTC Night Falls ~ Evening Star and Ran's theme with her ghostly field club one."
+		"YukariCtcMod": "YukariCtc (07) Mod that replaces Yukari's PCB theme with the CTC Night Falls ~ Evening Star and Ran's theme with her ghostly field club one.",
+		"YuukaMod": "YuukaMod (06) Mod that makes Yuuka playable in EoSD Embodiment of Scarlet Devil"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/4ChanMeta/touhou-patches/main/"
